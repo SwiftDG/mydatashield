e26faf7a3eb5f43c65100dcb23352e2e1601dcc3
SwiftDG <israelizuchukwu17382302@gmail.com>
2025-11-14 16:41:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+
 
 // Get directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
